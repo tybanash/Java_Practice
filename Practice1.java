70a26701e692d09cf545f00650427c036d2a77c8
Awah Nadege <awahnadege02@gmail.com>
2020-12-03 23:32:14 -0500

    Added file Practice 1
    Java program that prints my name and the programming language I am learning.

---
+++ Practice1.java
@@ -0,0 +1,12 @@
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		String name = "Nadege";
+		String language = "Java";
+		
+		System.out.println("Hello my name is " + name + ". I am learning " + language);
+
+	}
+
+}
